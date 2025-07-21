d618ce12df4598dcc5489634fe7b1f10075fc60f
sinyaw <sinyaw@acm.org>
2025-07-20 23:09:17 -0700

    parameterized dispatcher

--- bidexamples.js
+++ bidexamples.js
@@ -7,59 +7,61 @@ function bidExamples(choice) {
     disp.appendChild(gridDisp)
     var d = new Deck;
     var board = new Board(d);
-    if (['HCP', 'DP', 'TP'].includes(choice.value)) {
-        simplePoints(gridDisp, choice.value, board);
-    } else if (choice.value == 'LTC') {
-        ltcExamples(gridDisp, choice.value, board);
+    var dispatch = {'HCP': [ptSelector, ptAnswerer, [10, 15, 22]],
+        'DP': [ptSelector, ptAnswerer, [10, 15, 22]],
+        'TP': [ptSelector, ptAnswerer, [10, 15, 22]],
+        'LTC': [ltcSelector, ptAnswerer, [10, 7, 5]]
     }
+    if (choice.value in dispatch) 
+        simplePoints(gridDisp, choice.value, board, dispatch[choice.value]);
 }
 
-function ltcExamples(eDiv, k, board) {
-    var thresholds = [10, 7, 5];
+function simplePoints(eDiv, k, board, funcTbl) {
     var idx = 1
-    for (const t of thresholds) {
-        var nExamples = 5
-        while (nExamples > 0) {
-            board.deal();
-            for (const h of board.seats)
-                if (h[k] <= t && nExamples > 0) {
-                    gridElement(eDiv, idx.toString() + ":", 1, idx);
-                    gridElement(eDiv, h.toString(), 2, idx);
-                    var ans = gridElement(eDiv, k + "=" + h[k], 3, idx);
-                    ans.setAttribute("id", "Ans"+idx)
-                    ans.style['visibility'] = 'hidden'
-                    --nExamples;
-                    ++idx;
-                }
+    var nCap = 10;
+    var nExamples = nCap
+    while (nExamples > 0) {
+        board.deal();
+        for (const h of board.seats) {
+            if (funcTbl[0](h, k, Math.trunc(idx / (nCap / funcTbl[2].length)), funcTbl[2])) {
+                gridElement(eDiv, idx.toString() + ":", 1, idx);
+                gridElement(eDiv, h.toString(), 2, idx);
+                var ans = gridElement(eDiv, funcTbl[1](h, k, idx, funcTbl[2]), 3, idx);
+                ans.setAttribute("id", "Ans"+idx)
+                ans.style['visibility'] = 'hidden'
+                --nExamples;
+                ++idx;
+            }
+            if (nExamples <= 0)
+                break
         }
     }
     eDiv.addEventListener('click', (e) => {divKeyEvent(e)})
 }
-function simplePoints(eDiv, k, board) {
-    var thresholds = [10, 15, 22];
-    var idx = 1
-    for (const t of thresholds) {
-        var nExamples = 5
-        while (nExamples > 0) {
-            board.deal();
-            for (const h of board.seats)
-                if (h[k] >= t && nExamples > 0) {
-                    gridElement(eDiv, idx.toString() + ": ", 1, idx);
-                    gridElement(eDiv, h.toString(), 2, idx);
-                    var ans = gridElement(eDiv, k + "=" + h[k], 3, idx);
-                    ans.setAttribute("id", "Ans"+idx)
-                    ans.style['visibility'] = 'hidden'
-                    --nExamples;
-                    ++idx;
-                }
-        }
-    }
-    eDiv.addEventListener('click', (e) => {divKeyEvent(e)})
+function ptSelector(h, k, idx, data) {
+    var ret = false;
+    if (idx < data.length - 1)
+        ret = h[k] >= data[idx] && h[k] < data[idx+1];
+    else
+        ret = h[k] >= data[data.length-1];
+    return ret;
+}
+
+function ptAnswerer(h, k, idx, data) {
+    return k + "=" + h[k].toString();
+}
+function ltcSelector(h, k, idx, data) {
+    var ret = false;
+    if (idx < data.length - 1)
+        ret = h[k] <= data[idx] && h[k] > data[idx+1];
+    else
+        ret = h[k] <= data[data.length-1];
+    return ret;
 }
 
 function divKeyEvent(e) {
     var idx = 1
-    while (idx <= 15) {
+    while (idx <= 10) {
         var e = document.getElementById('Ans' + idx)
         if (e !== null) {
             if (e.style['visibility'] != 'hidden')
