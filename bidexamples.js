ab37fd2be9b03132856e3b5afb2d6f2c08d24a25
sinyaw <sinyaw@acm.org>
2025-07-22 22:38:53 -0700

    opening examples

--- bidexamples.js
+++ bidexamples.js
@@ -1,40 +1,64 @@
-
-function bidExamples(eDiv, k, board, funcTbl) {
-    eDiv.style['grid-template-columns'] = "3vw 20vw 70vw";
-    var idx = 1;
-    var nCap = 10;
-    var nExamples = nCap;
-    while (nExamples > 0) {
-        board.deal();
-        hIdx = funcTbl[0](board);
-        if (hIdx >= 0) {
-            var item = gridElement(eDiv, idx.toString() + ":", 1, idx);
-            item.style["justify-self"] = "right";
-            gridElement(eDiv, board.seats[hIdx].toString(), 2, idx);
-            var des = gridElement(eDiv, handDescription(board.seats[hIdx]), 3, idx);
-            des.setAttribute("id", "Ans"+idx);
-            --nExamples;
-            ++idx;
-        }
+class BidEx extends Qualifier {
+    MenuItems = [
+        {'Name': 'Open', 'Cases': [{"BidSeq":[]}]}
+    ];
+    constructor() {super();}
+    init(e) {
+        clearContents(e);
+        e.style['grid-template-columns'] = "3vw 20vw 5vw 50vw";
+        this.nCap = 10;
+        this.disp = e;
     }
+
+    run(k) {
+        var nExamples = this.nCap;
+        var idx = 1;
+        while (nExamples > 0) {
+            this.BridgeBoard.deal();
+            var found = null;
+            var openIdx = {'1x': 0, '1NT': 1, '2C': 2, 'Preempt': 3}
+            if (k in openIdx)
+                found = this.selectOpen(openIdx[k]);
+            if (found && found.RetStatus) {
+                let item = gridElement(this.disp, idx.toString() + ":", 1, idx);
+                item.style["justify-self"] = "right";
+                gridElement(this.disp, this.BridgeBoard.seats[found.Seat].toString(), 2, idx);
+                let hint = gridElement(this.disp, this.htmlBid(found.Bid), 3, idx)
+                hint.setAttribute('id', "Hint"+idx)
+                hint.style['visibility'] = 'hidden';
+                let ansText = handDescription(this.BridgeBoard.seats[found.Seat]);
+                let ans = gridElement(this.disp, ansText, 4, idx);
+                ans.setAttribute("id", "Ans"+idx);
+                ans.style['visibility'] = 'hidden';
+                --nExamples;
+                ++idx;
+            }
+        }
+        this.disp.addEventListener('click', (e) => {divKeyEvent(e)})
+    }
+
+    selectOpen(i) {
+        var pItem = this.MenuItems[0]['Cases'][0];
+        var found = this.findQualifiedBoard(pItem);
+        var choices = [['1S', '1H', '1D', '1C'],
+                    ['1NT'], ['2C'], ['2S', '2H', '2D', '3C', '3S', '3H', '3D']];
+        if (found && found.RetStatus && choices[i].includes(found.Bid))
+            return found;
+        return null;
+    }
+
 }
 
-
-function selectOpen(board) {
-    var bestHand = 0;
-    var maxPt = 0;
-    for (var i = 0; i < board.seats.length; ++i) {
-        if (board.seats[i].HCP > maxPt) {
-            maxPt = board.seats[i].HCP;
-            bestHand = i;
-        }
-    }
-    return bestHand;
+function bidExamples(eDiv, k) {
+    var exer = new BidEx();
+    exer.init(eDiv);
+    exer.run(k)
 }
 
 function select2C(board) {
     return 0;
 }
+
 function handDescription(hand) {
     str = "HCP: " + hand.HCP.toString();
     str += ", DP: " + hand.DP.toString();
--- exerdriver.js
+++ exerdriver.js
@@ -8,28 +8,29 @@ function exercies(choice) {
     gridDisp.setAttribute("id", "PointExamples")
     gridDisp.setAttribute("class", "PointExamples")
     disp.appendChild(gridDisp)
-    var d = new Deck;
-    var board = new Board(d);
     var ptDispatch = {'HCP': [hcpSelector, hcpAnswerer, [10, 15, 22]],
         'DP': [dpSelector, tagAnswerer, [10, 15, 22]],
         'LTC': [ltcSelector, tagAnswerer, [8, 5, 3]]
     }
-    var bidDispatch = {'Open': [selectOpen], '2C': [select2C]}
-    if (choice.value in ptDispatch) 
+    if (choice.value in ptDispatch) {
+        let d = new Deck;
+        let board = new Board(d);
         ptExamples(gridDisp, choice.value, board, ptDispatch[choice.value]);
-    else if (choice.value in bidDispatch)
-        bidExamples(gridDisp, choice.value, board, bidDispatch[choice.value]);
+    } else 
+        bidExamples(gridDisp, choice.value);
 }
 
 function divKeyEvent(e) {
     var idx = 1
     while (idx <= 10) {
-        var e = document.getElementById('Ans' + idx)
-        if (e !== null) {
-            if (e.style['visibility'] != 'hidden')
+        var eAns = document.getElementById('Ans' + idx)
+        var eHint = document.getElementById('Hint' + idx)
+        if (eAns !== null) {
+            if (eAns.style['visibility'] != 'hidden')
                 ++idx
             else {
-                e.style['visibility'] = 'visible';
+                eAns.style['visibility'] = 'visible';
+                eHint.style['visibility'] = 'visible';
                 break
             }
         }
