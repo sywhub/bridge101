a1c4dc9058c8c39bec5ce2cb49bc0c08530fda2e
sinyaw <sinyaw@acm.org>
2025-07-28 22:34:31 -0700

    deployment

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+files=`grep 'src=' index.html  | sed -e 's/.*="//' -e 's/".*$//' -e '/refcard2/d'`
+files="index.html ${files}"
+tmp=`mktemp -p . -t ftp`
+echo "cd js/ssc_examples" > $tmp
+echo "rm *" >> $tmp
+for f in ${files}
+do
+echo "put $f" >> $tmp
+done
+echo "chmod 0644 *" >> $tmp
+echo "quit" >> $tmp
+sftp -p -b $tmp -N u47659892@ftp.nomadicminds.org 
+rm $tmp
