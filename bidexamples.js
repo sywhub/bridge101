518fd036765fc9f29c7e1252725117ffa58fd86c
sinyaw <sinyaw@acm.org>
2025-07-31 10:19:35 -0700

    tweak

--- bidexamples.js
+++ bidexamples.js
@@ -7,11 +7,11 @@ class BidEx extends Qualifier {
         '1NT': {"BidSeq":[], 'Expects': ['1NT'], 'Novice': 15},
         '2C': {"BidSeq":[], 'Expects': ['2C'], 'Novice': 22},
         'Preempt': {"BidSeq":[], 'Expects': ['2S', '2H', '2D', '3C', '3S', '3H', '3D']},
-        '-': null,
+        '-': null,  // divider
         '1M Reply': [{"BidSeq":['1S', '-']},{"BidSeq":['1H', '-']}],
-        '1NT Reply': [{"BidSeq":['1NT', '-']}],
-        '2C Reply': [{"BidSeq":['2C', '-']}],
+        '2C Reply': {"BidSeq":['2C', '-']},
         '1m Reply': [{"BidSeq":['1D', '-']},{"BidSeq":['1C', '-']}],
+        '1NT Reply': {"BidSeq":['1NT', '-']},
         'Preempt Reply': [{"BidSeq":['2S', '-']},
             {"BidSeq":['2H', '-']}, {"BidSeq":['2D', '-']}]
     };
@@ -23,10 +23,12 @@ class BidEx extends Qualifier {
         this.disp = e;
     }
 
+    /* Generate display items */
     run(k) {
         var nExamples = this.nCap;
         var idx = 1;
         while (nExamples > 0) {
+            // workhorse function to pick what/whether to display
             var found = this.select(k);
             if (found && found.RetStatus) {
                 let item = gridElement(this.disp, idx.toString() + ":", 1, idx);
@@ -49,10 +51,13 @@ class BidEx extends Qualifier {
         }
     }
 
+    // decide whether/what to display
     select(k) {
         var pItem = BidEx.MenuItems[k];
-        if (pItem.length > 1)
+        // if there are multple, pick a random one
+        if (Array.isArray(pItem))
             pItem = pItem[Math.trunc(Math.random() * pItem.length)];
+        // pItem must be an object with "BidSeq" key
         var found = this.findQualifiedBoard(pItem);
 
         if (!found || !found.RetStatus)
