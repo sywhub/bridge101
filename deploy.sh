09d16c92476eda8a8f4e14637b92175fc4ddfdcf
sinyaw <sinyaw@acm.org>
2026-03-01 17:12:10 -0800

    add new files

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 files=` grep 'script src=\"' index.html | sed -e 's/.*="//' -e 's/".*$//'`
 tmpindex=`mktemp -p . -t html`
-sed -e 's/src=\"\.\.\/refcard2\//src="/' index.html > $tmpindex
+sed -e 's/src=\"\.\.\/refcard2\//src="/' index.html > ${tmpindex}.1
+sed -e 's/src=\"\.\.\/refcard2\//src="/' bidExercises.html > ${tmpindex}.2
 tmpftp=`mktemp -p . -t ftp`
 echo "cd js/`basename $(PWD)`" > $tmpftp
 echo "rm *" >> $tmpftp
-echo "put $tmpindex index.html" >> $tmpftp
+echo "put ${tmpindex}.1 index.html" >> $tmpftp
+echo "put ${tmpindex}.2 bidExercises.html" >> $tmpftp
 for f in ${files}
 do
 echo "put $f ." >> $tmpftp
@@ -13,6 +15,8 @@ done
 echo "chmod 0644 *" >> $tmpftp
 echo "quit" >> $tmpftp
 sftp -p -b $tmpftp -N u47659892@ftp.nomadicminds.org 
-rm $tmpindex
+rm ${tmpindex}
+rm ${tmpindex}.1
+rm ${tmpindex}.2
 rm $tmpftp
-git push --all origin
+#git push --all origin
